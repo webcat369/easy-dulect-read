e873d18ac61fd8816f857d092d93d9597445f963
哑戏 <2541873074@qq.com>
2020-10-08 15:28:28 +0800

    整理rem实现弹性布局

--- easy-dulect-read/src/api/network.js
+++ easy-dulect-read/src/api/network.js
@@ -5,21 +5,42 @@ axios.defaults.baseURL = 'http://localhost:3000'
 axios.defaults.timeout = 3000
 
 // 添加请求拦截器
-axios.interceptors.request.use(function (config) {
+axios.interceptors.request.use((config) => {
   // 在发送请求之前做些什么
   return config
-}, function (error) {
+}, (error) => {
   // 对请求错误做些什么
   return Promise.reject(error)
 })
 
 // 添加响应拦截器
-axios.interceptors.response.use(function (response) {
+axios.interceptors.response.use((response) => {
   // 对响应数据做点什么
   return response
-}, function (error) {
+}, (error) => {
   // 对响应错误做点什么
-  return Promise.reject(error)
+  const { response } = error
+  if (response) {
+    // 响应拦截失败，但仍然返回了结果
+    switch (response.status) {
+      case 401:
+        console.log('未登录，请先登录')
+        break
+      case 403: // 服务器理解了该请求，但拒绝了执行它，(一般是TOKEN过期)
+        localStorage.removeItem('token')
+        break
+      case 404:
+        console.log('请求失败,当前接口不存在')
+        break
+    }
+  } else {
+    // 服务器连结果都没有返回，存在两种结果：1.断网；2.服务器崩了
+    if (!window.navigator.onLine) {
+      console.log('断网了')
+      return
+    }
+    return Promise.reject(error)
+  }
 })
 
 export default {
